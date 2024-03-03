511fd656a420e0ad2989287e6e7a0bf1ff1bc336
Mrumman-75 <siamahmed166815@gmail.com>
2024-03-03 05:57:23 -0500

    first upload

---
+++ customize.js
@@ -0,0 +1,7 @@
+function play(){
+    let homeSection = document.getElementById('homePages')
+    homeSection.classList.add('hidden')
+    
+    let playG = document.getElementById('playground')
+    playG.classList.remove('hidden')
+}
